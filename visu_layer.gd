8073f81451d24085ee019f6e116183b912400bf3
b8horpet <b8horpet@gmail.com>
2025-06-25 19:55:07 +0200

    visu can be toggled between food and poison values

--- visu_layer.gd
+++ visu_layer.gd
@@ -4,11 +4,15 @@ extends TileMapLayer
 @onready var cell_grid : CellGrid = $"../Cells"
 
 var cells : Dictionary[Vector2i,Visu] = {}
-var prop : String = "poison"
+var props : Array[String] = ["food","poison"]
+var prop_idx := 0
 
 func _process(delta: float) -> void:
-	if prop.is_empty():
+	if props.is_empty():
 		return
+	if prop_idx < 0 or prop_idx >= props.size():
+		return
+	var prop = props[prop_idx]
 	if prop in cell_grid.data:
 		clear()
 		var pd := cell_grid.data[prop]
@@ -17,3 +21,9 @@ func _process(delta: float) -> void:
 		update_internals()
 		for c in pd:
 			cells[c].display(pd[c])
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var ke := event as InputEventKey
+		if ke.keycode == Key.KEY_TAB and ke.pressed and !ke.echo:
+			prop_idx = (prop_idx + 1) % props.size()
